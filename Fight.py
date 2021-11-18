c03f8b9b557f34f474a6690bb50df48b9bc033d6
Pierre Pinet <pierre.pinet@etu.u-bordeaux.fr>
2021-11-18 23:18:48 +0100

    User vs AI is now possible

--- Fight.py
+++ Fight.py
@@ -4,6 +4,49 @@ from Heuristics import *
 from MinMax import *
 
 
+def userVSminmax(board,minMaxHeuristic,minMaxDepth):
+    print(board)
+
+    while not board.is_game_over():
+
+        print("----------")
+        print("User turn")
+
+        legalMove = False
+        while not legalMove:
+
+            validInput = False
+            while not validInput:
+
+                try:
+                    start = str(input())
+                    end = str(input())
+                    #cant promote yet
+                    userMove = chess.Move(chess.parse_square(start),chess.parse_square(end))
+                    validInput = True
+                except ValueError:
+                    print("invalid input")
+
+            if userMove in board.generate_legal_moves():
+                board.push(userMove)
+                legalMove = True
+            else:
+                print("illegal move")
+
+        print(board)
+
+        if not board.is_game_over():
+
+            print("----------")
+            print("AI turn")
+            minMax2Move = getBestMove(board,chess.BLACK,minMaxHeuristic,minMaxDepth)
+            board.push(minMax2Move)
+            print(board)
+
+    print(board.result())
+
+    return board
+
 
 def minmaxVSminmax(board,minMaxHeuristic1,minMaxHeuristic2,minMaxDepth1,minMaxDepth2):
     print(board)
@@ -23,7 +66,7 @@ def minmaxVSminmax(board,minMaxHeuristic1,minMaxHeuristic2,minMaxDepth1,minMaxDe
             minMax2Move = getBestMove(board,chess.BLACK,minMaxHeuristic2,minMaxDepth2)
             board.push(minMax2Move)
             print(board)
-            
+
     print(board.result())
 
     return board
--- main.py
+++ main.py
@@ -6,5 +6,4 @@ from Fight import *
 
 
 board = chess.Board()
-board.set_piece_at(chess.parse_square("e3"),chess.Piece(chess.ROOK,chess.WHITE))
-minmaxVSminmax(board,basicHeuristic,basicHeuristic,2,2)
+userVSminmax(board,basicHeuristic,2)
